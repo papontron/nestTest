60a716b3a15bc723a6d0c9f108e741a67dd63f4c
papontron <70109968+papontron@users.noreply.github.com>
2024-12-08 22:28:42 -0500

    port configured

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,8 @@
-export class CreateProductDto {}
+import { Category } from '../../categories/entities/category.entity';
+
+export class CreateProductDto {
+  name: string;
+  price: number;
+  stock: number;
+  category: Category;
+}
